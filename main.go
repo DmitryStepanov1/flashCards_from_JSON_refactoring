caeafcdb177da682b688d6cc6a8f06d1f6512a21
DmitryStepanov1 <ds.dstepanov@gmail.com>
2024-03-28 18:47:36 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// checks if file is found and return it's format or error
+func fileValidation(fileName string) (bool, string) {
+
+	// get file metaData
+	fileInfo, err := os.Stat(fileName)
+
+	// split file-name and it's format
+	fileNameParts := strings.Split(fileName, ".")
+
+	// check errors
+	if os.IsNotExist(err) {
+		fmt.Println("file not found")
+		return false, ""
+	} else if err != nil {
+		fmt.Println("error:", err)
+		return false, ""
+	} else if fileInfo.Size() == 0 {
+		fmt.Println("file is empty")
+		return false, ""
+	} else if len(fileNameParts) == 1 {
+		fmt.Println("file has no format")
+		return false, ""
+	}
+
+	// possible file types
+	textFileExtensions := map[string]bool{
+		"json": true,
+		// new formats can be added here
+	}
+
+	// check file's format
+	if textFileExtensions[fileNameParts[len(fileNameParts)-1]] {
+		return true, fileNameParts[len(fileNameParts)-1]
+	}
+
+	fmt.Println("unsupported file format. Please use any of next formats: TXT, CSV or JSON")
+	return false, ""
+
+}
+
+// checks if file contatins valid JSON data and can be used for dictionary
+func jsonValidation(inputString string) (inputMap map[string]string) {
+
+	//v bool, byteValue []byte, err error
+
+	byteValue, err := os.ReadFile(inputString)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Successfully read json-file")
+	}
+
+	err = json.Unmarshal([]byte(byteValue), &inputMap)
+	if err != nil || len(inputMap) == 0 {
+		fmt.Println(err)
+		fmt.Println("Data from JSON wasn't parsed")
+	}
+
+	return inputMap
+
+}
+
+// provides dictation from map for user and exits the program when finish
+func dictation(m map[string]string) {
+
+	for {
+
+		scanner := bufio.NewScanner(os.Stdin)
+		v := randomWord(m)
+
+	jumpTo:
+
+		fmt.Print("> ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Check if the user wants to finish dictation
+		if input == "exit" {
+			break
+		} else if input == v {
+			fmt.Println("Correct! Try next word")
+			continue
+		} else {
+			fmt.Println("Wrong, try again")
+			goto jumpTo
+		}
+
+	}
+
+}
+
+// checks if any dictionary word is Cyrillic Or Latin and has no spaces
+func containsCyrillicOrLatin(s string) bool {
+	for _, r := range s {
+		if unicode.Is(unicode.Cyrillic, r) || unicode.Is(unicode.Latin, r) {
+			return true
+		}
+	}
+	return false
+}
+
+// provides random word from map for dictation
+func randomWord(m map[string]string) string {
+	//k := rand.Intn(len(m))
+
+	for i, v := range m {
+		s := fmt.Sprintf("Переведи %s:", i)
+		fmt.Println(s)
+		return v
+	}
+
+	return ""
+
+}
+
+func main() {
+
+	var m map[string]string
+	// Create a new scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+jumpTo:
+
+	fmt.Println("Enter file to parse:")
+
+	// Scan for the next token (which is a line)
+	scanner.Scan()
+
+	inputString := scanner.Text()
+
+	if scanner.Text() == "exit" {
+		os.Exit(0)
+	}
+
+	fValid, _ := fileValidation(inputString)
+
+	if fValid == false {
+		goto jumpTo
+	}
+
+	m = jsonValidation(inputString)
+
+	if len(m) == 0 {
+		goto jumpTo
+	}
+
+	fmt.Println("You've uploaded next words from dictionary:")
+	for key, value := range m {
+		fmt.Printf("%s: %s\n", key, value)
+	}
+	fmt.Println("Now the dictation starts.")
+
+	dictation(m)
+
+	fmt.Println("It was a pleasure to work with you, see ya!")
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Flashcards from JSON file
+
+It parses JSON-file and stores valid values to map. Then it asks you fill translation to the terminal.
+
+With tests and refactoring.
